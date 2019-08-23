a79687ffa919ae142ff22d46bf33ab6ef659c155
xiayongchao <2388604953@qq.com>
2019-08-23 19:22:13 +0800

    优化

---
+++ collapsar-test/src/main/java/org/jc/test/collapsar/caches/CommonCaches.java
@@ -0,0 +1,12 @@
+package org.jc.test.collapsar.caches;
+
+import org.jc.framework.collapsar.annotation.MultiCaches;
+
+/**
+ * @author xiayc
+ * @date 2019/8/23
+ */
+@MultiCaches
+public class CommonCaches {
+
+}
